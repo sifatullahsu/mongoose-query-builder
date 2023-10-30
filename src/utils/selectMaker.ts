7b601a1e1b3755da926b9dbc94eaee9072866557
sifatullahsu <personal.sifat@gmail.com>
2023-10-30 20:57:43 +0600

    select bug fixed

--- src/utils/selectMaker.ts
+++ src/utils/selectMaker.ts
@@ -3,7 +3,16 @@ const selectMaker = (input: string | string[], authorizedSelect: string[]): stri
   if (Array.isArray(input)) throw new Error('Multiple select found.')
 
   const inputArray = input.split(',')
-  const result = inputArray.filter((element: string) => !authorizedSelect.includes(element))
+  const result = inputArray.filter((element: string) => {
+    const trimmedElement = element.trim()
+    // Exclude elements that are not in authorizedSelect and do not contain spaces or empty strings
+    return (
+      !authorizedSelect.includes(trimmedElement) &&
+      trimmedElement !== '' &&
+      !/\s/.test(trimmedElement) &&
+      !trimmedElement.includes('+')
+    )
+  })
 
   return result.join(' ')
 }
