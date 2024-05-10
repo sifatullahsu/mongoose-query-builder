f0a61757d3e9852d10e30c846367f965c438efaf
sifatullahsu <personal.sifat@gmail.com>
2024-05-10 18:41:41 +0600

    pagination

--- src/app/pagination.ts
+++ src/app/pagination.ts
@@ -4,14 +4,11 @@ export const pagination: TPagination = q => {
   if (Array.isArray(q.page)) throw new Error('Multiple `page` found.')
   if (Array.isArray(q.limit)) throw new Error('Multiple `limit` found.')
   if (Array.isArray(q.sort)) throw new Error('Multiple `sort` found.')
+  if (Array.isArray(q.skip)) throw new Error('Multiple `skip` found.')
 
-  let page = parseInt(q.page as string, 10) ?? 1
-  let limit = parseInt(q.limit as string, 10) ?? 10
-
-  page = Math.max(1, isNaN(page) ? 1 : page)
-  limit = Math.max(1, isNaN(limit) ? 10 : limit)
-
-  const skip = (page - 1) * limit
+  const page = Number(q.page) || 1
+  const limit = Number(q.limit) || 20
+  const skip = (Number(q.skip) || 0) + (page - 1) * limit
   const sort = q.sort ? (q.sort as string).replace(/,/g, ' ') : 'createdAt'
 
   return { page, limit, skip, sort }
