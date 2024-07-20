881d30a2862a55fa01956edf99a571cc1f75a24f
SIFAT ULLAH <personal.sifat@gmail.com>
2024-07-20 16:49:14 +0600

    pagination

--- src/app/pagination.ts
+++ src/app/pagination.ts
@@ -1,10 +1,10 @@
 import { TPagination } from '../types'
 
 export const pagination: TPagination = (q, paginationRules) => {
-  const page = q.page || paginationRules?.page || 1
-  const limit = q.limit || paginationRules?.limit || 20
-  const skip = (q.skip || paginationRules?.skip || 0) + (page - 1) * limit
-  const sort = q.sort ? q.sort.replace(/,/g, ' ') : paginationRules?.sort || 'createdAt'
+  const page = q?.page || paginationRules?.page || 1
+  const limit = q?.limit || paginationRules?.limit || 20
+  const skip = (q?.skip || paginationRules?.skip || 0) + (page - 1) * limit
+  const sort = q?.sort ? q.sort.replace(/,/g, ' ') : paginationRules?.sort || 'createdAt'
 
   return { page, limit, skip, sort }
 }
